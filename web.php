9a1eaaf9b28c1100a298be45372793dccc94f726
DunikAndriyani <147916679+DunikAndriyani@users.noreply.github.com>
2025-09-09 01:38:05 +0700

    data via controller

--- web.php
+++ web.php
@@ -1,17 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
-    return view ('home');
-});
-
-Route::view('/about', 'about', [
-    'name' => 'Dunik Andriyani',
-    'npm' => '2310631170134',
-    'prodi' => 'Informatika',
-]);
+Route::get('/about', [AboutController::class, 'index']);
