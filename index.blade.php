ad50c8c098646e1087f08931d9c25ee782459aa1
DunikAndriyani <147916679+DunikAndriyani@users.noreply.github.com>
2025-09-09 01:41:04 +0700

    data via controller

---
+++ index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Page</title>
+</head>
+<body>
+    <h1>About Me</h1>
+    <p>Nama : {{ $data['nama'] }}</p>
+    <p>NIM  : {{ $data['npm'] }}</p>
+    <p>Prodi: {{ $data['prodi'] }}</p>
+</body>
+</html>
