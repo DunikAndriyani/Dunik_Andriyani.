9999eff3a437446e49f5d01e3b5b974184bf43b6
DunikAndriyani <147916679+DunikAndriyani@users.noreply.github.com>
2025-09-09 00:51:59 +0700

    data via route

---
+++ web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view ('home');
+});
+
+Route::view('/about', 'about', [
+    'name' => 'Dunik Andriyani',
+    'npm' => '2310631170134',
+    'prodi' => 'Informatika',
+]);
